c9e92b117f5e93952130ece6c0dd5bf0074296a4
VASANTH KUMAR THULASIDASS <69359978+vasanthkumart007@users.noreply.github.com>
2022-03-08 07:59:18 +0530

    Create dep.sh

---
+++ image-deployment/actions/deploy-image/dep.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "--------------------INSIDE DEPLOYMENT SCRIPT--------------------"
+platform=$1
+env=$2
+
+
+echo "Platform    -------> $platform"
+echo "Environment -------> $env"
+
+
+echo "--------------------INSIDE DEPLOYMENT SCRIPT--------------------"
